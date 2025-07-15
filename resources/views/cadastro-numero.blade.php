1f03d0b480cd66e813ac42e2e3bc9f554921a95c
Britto <britto@MacBook-Air-de-LabMaker.local>
2025-07-15 11:00:43 -0300

    Tentando colocar o sweetAlert e traduzindo as telas

--- resources/views/cadastro-numero.blade.php
+++ resources/views/cadastro-numero.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
    <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-         {{ __('Dashboard') }}
+         {{ __('Cadastro de Número') }}
       </h2>
    </x-slot>
 
@@ -35,19 +35,18 @@
             </form>
          </div>
       </div>
-   </div>
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
+         @if(session('success'))
+         <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            <span class="block sm:inline">{{ session('success') }}</span>
+         </div>
+         @endif
 
-   <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
-      @if(session('success'))
-      <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-         <span class="block sm:inline">{{ session('success') }}</span>
+         @if(session('error'))
+         <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <span class="block sm:inline">{{ session('error') }}</span>
+         </div>
+         @endif
       </div>
-      @endif
-
-      @if(session('error'))
-      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-         <span class="block sm:inline">{{ session('error') }}</span>
-      </div>
-      @endif
    </div>
 </x-app-layout>
